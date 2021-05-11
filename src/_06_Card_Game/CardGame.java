baeb91509803b113712a6d27e542b78e83be22f5
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-11 16:56:48 -0700

    04/11/2021
    started on Go Fish game!

---
+++ src/_06_Card_Game/CardGame.java
@@ -0,0 +1,9 @@
+package _06_Card_Game;
+
+public class CardGame {
+
+	public static void main(String[] args) {
+		new CardTable().run();
+	}
+
+}
---
+++ src/_06_Card_Game/Rank.java
@@ -0,0 +1,14 @@
+package _06_Card_Game;
+
+public enum Rank {
+	ACE(0), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(0), QUEEN(0), KING(0);
+	private int value;
+	
+	private Rank(int value) {
+		this.value = value;
+	}
+	int getValue() {
+		return value;
+	}
+	
+}
